f078638b80951277eaf63e2a62d44c3965431c24
sayemx <sayem.saqueeb@splashbi.com>
2025-08-12 17:55:19 +0530

    Section 9: Gateway server, created custom routes in the main application class using java configuration, and 2 filters to create and add correlation id in the response header.

--- src/main/java/com/sayem/gateway/GatewayServerApplication.java
+++ src/main/java/com/sayem/gateway/GatewayServerApplication.java
@@ -1,13 +1,46 @@
 package com.sayem.gateway;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+	
+	
+	@Bean
+	public RouteLocator sayemRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/sayem/accounts/**")
+						.filters(f -> f
+								.rewritePath("/sayem/accounts/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								)
+						.uri("lb://ACCOUNTS"))
+				.route(p -> p
+						.path("/sayem/loans/**")
+						.filters(f -> f
+								.rewritePath("/sayem/loans/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								)
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/sayem/cards/**")
+						.filters(f -> f
+								.rewritePath("/sayem/cards/(?<segment>.*)", "/${segment}")
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								)
+						.uri("lb://CARDS"))
+				.build();
 	}
 
 }
